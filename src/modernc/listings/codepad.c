e76e13d8e0c6bd2b8583ebf209e54d040276a0de
fredy.diaz <fredy.diaz@2degrees.nz>
2025-07-09 18:52:14 +1200

    style: improve formatting

--- src/modernc/listings/codepad.c
+++ src/modernc/listings/codepad.c
@@ -1,19 +1,24 @@
 #include <limits.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-enum corvid { magpie, raven, jay, chough, corvid_num, };
+enum corvid {
+  magpie,
+  raven,
+  jay,
+  chough,
+  corvid_num,
+};
 
 #define FLOCK_MAGPIE (1U << magpie)
 #define FLOCK_RAVEN (1U << raven)
 #define FLOCK_JAY (1U << jay)
 #define FLOCK_CHOUGH (1U << chough)
 #define FLOCK_EMPTY 0U
-#define FLOCK_FULL ((1U << corvid_num)-1)
+#define FLOCK_FULL ((1U << corvid_num) - 1)
 
-int main(void)
-{
-    int x = INT_MIN + INT_MAX;
-    printf("%d\n", __builtin_popcount(x));
-    return EXIT_SUCCESS;
+int main(void) {
+  int x = INT_MIN + INT_MAX;
+  printf("%d\n", __builtin_popcount(x));
+  return EXIT_SUCCESS;
 }
