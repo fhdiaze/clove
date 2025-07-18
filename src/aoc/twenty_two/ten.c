97117f89de4b2523037a0b6b82747e5ef52e637f
fredy.diaz <fredy.diaz@2degrees.nz>
2025-07-18 15:18:49 +1200

    feat: testing funcs

--- src/aoc/twenty_two/ten.c
+++ src/aoc/twenty_two/ten.c
@@ -2,24 +2,24 @@
 #include <stdlib.h>
 
 int main(void) {
-  int clock = 0;
-  int instruction_len = 256;
-  char instruction[instruction_len];
+    int clock = 0;
+    int instruction_len = 256;
+    char instruction[instruction_len];
 
-  FILE *input_file = fopen("./aoc/twenty_two/ten.txt", "r");
-  if (input_file == NULL) {
-    printf("Unable to find the file");
-    return -1;
-  }
+    FILE *input_file = fopen("./aoc/twenty_two/ten.txt", "r");
+    if (input_file == NULL) {
+        printf("Unable to find the file");
+        return -1;
+    }
 
-  while (fgets(instruction, instruction_len, input_file) != NULL) {
-    clock++;
-    printf("%s, %d", instruction, clock);
-  }
+    while (fgets(instruction, instruction_len, input_file) != NULL) {
+        clock++;
+        printf("%s, %d", instruction, clock);
+    }
 
-  fclose(input_file);
+    fclose(input_file);
 
-  char const *const t[3];
+    char const *const t[3];
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
